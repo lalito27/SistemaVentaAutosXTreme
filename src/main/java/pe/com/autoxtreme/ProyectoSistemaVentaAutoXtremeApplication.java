f2826c78966bfbfa7e62ce715b29898b4a77b494
Eduardo Andres <lalitorap1917@gmail.com>
2023-05-04 22:16:09 -0500

    Comienzo del Proyecto Inicial Base

---
+++ src/main/java/pe/com/autoxtreme/ProyectoSistemaVentaAutoXtremeApplication.java
@@ -0,0 +1,13 @@
+package pe.com.autoxtreme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSistemaVentaAutoXtremeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSistemaVentaAutoXtremeApplication.class, args);
+	}
+
+}
